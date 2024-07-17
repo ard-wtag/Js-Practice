18ad393b00e203dcd4491aecefaef016460bfec5
ard-wtag <arijit.diganto@welldev.io>
2024-07-17 09:35:38 +0600

    Clear Cart
    Everything works now

--- script.js
+++ script.js
@@ -191,4 +191,4 @@ cartBtn.addEventListener("click", () => {
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
 
-this.clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
